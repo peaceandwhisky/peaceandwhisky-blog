5ff93b20cf277171ebf0ad2bc05374bc66156fb3
peaceandwhisky <pioneerandf@gmail.com>
2023-01-07 14:07:37 +0900

    tailwindのテンプレートを適用

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,82 @@
-// pages/index.js
-import Link from "next/link";
+import Link from '@/components/Link'
+import formatDate from '@/lib/utils/formatDate'
 import { client } from "../libs/client";
 
-export default function Home({ blog }) {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">peaceandwhisky</h1>
-      <p>以下記事一覧です。</p>
-      <ul>
-        {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blogs/${blog.id}`}>
-              {blog.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+const MAX_DISPLAY = 5
 
-// データをテンプレートに受け渡す部分の処理を記述します
-export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blogs" });
+export async function getStaticProps() {
+  const posts = await client.get({ endpoint: "blogs" });
 
   return {
     props: {
-      blog: data.contents,
+      posts: posts.contents,
     },
   };
-};
+}
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          </p>
+        </div>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          {!posts.length && 'No posts found.'}
+          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+            const { id, publishedAt, title } = frontMatter
+            return (
+              <li key={id} className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={publishedAt}>{formatDate(publishedAt)}</time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blogs/${id}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h2>
+                        </div>
+                      </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={`/blogs/${id}`}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {posts.length > MAX_DISPLAY && (
+        <div className="flex justify-end text-base font-medium leading-6">
+          {/* <Link
+            href="/blogs"
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label="all posts"
+          >
+            All Posts &rarr;
+          </Link> */}
+        </div>
+      )}
+    </>
+  )
+}
